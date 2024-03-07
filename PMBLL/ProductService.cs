00c6c406ce9840237b8ed9886995cd1e5e3a4c2c
Alejandro Pedraza <luisalejandropedrazacubillos@student.mitt.ca>
2024-03-07 15:00:52 -0600

    Submiting the solution

---
+++ PMBLL/ProductService.cs
@@ -0,0 +1,42 @@
+﻿using PMDAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMBLL
+{
+    public class ProductService
+    {
+        public List<Product> GetProducts()
+        {
+            ProductRepository productRepository = new ProductRepository();
+            return productRepository.GetProducts();
+        }
+
+        public bool AddProduct(Product product)
+        {
+            ProductRepository productRepository = new ProductRepository();
+            return productRepository.AddProduct(product);
+        }
+        
+        public Product GetProduct(int productId)
+        {
+            ProductRepository productRepository = new ProductRepository();
+            return productRepository.GetProduct(productId);
+        }
+        
+        public bool EditProduct(Product product)
+        {
+            ProductRepository productRepository = new ProductRepository();
+            return productRepository.EditProduct(product);
+        }
+        
+        public bool DeleteProduct(int productId)
+        {
+            ProductRepository productRepository = new ProductRepository();
+            return productRepository.DeleteProduct(productId);
+        }
+    }
+}
